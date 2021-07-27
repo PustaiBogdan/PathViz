43bc75a680073a69bb9a985e12eeea79b172a734
Pustai Bogdan <pustaibogdan16@gmail.com>
2021-07-27 13:13:49 +0300

    desktop-git -final grid resp commit

--- redrawFirst.js
+++ redrawFirst.js
@@ -1,38 +1,39 @@
 function redrawFirst(){
-    // for (var i = 0; i < closedSet.length; i++) {
-    //   closedSet[i].show(color(0, 205, 0));
-    // }
-  
-    // for (var i = 0; i < openSet.length; i++) {
-    //   openSet[i].show(color(205, 205, 0));
-    // }
-    background(225, 225, 225);
-    for (var i=0;i<rows;i++){
-      for(var j=0; j<cols;j++){
-        if(!openSet.includes(grid[i][j]) && !closedSet.includes(grid[i][j])){
-          grid[i][j].show(color(225, 225, 225));
-          console.log(i,j);
-        }
-
+ 
+  background(225, 225, 225);
+  for (var i=0;i<rows;i++){
+    for(var j=0; j<cols;j++){
+      if(!openSet.includes(grid[i][j]) && !closedSet.includes(grid[i][j]) ){ 
+        grid[i][j].show(color(225, 225, 225));
+    
       }
+
     }
-
-    // for (var i=3;i<rows;i++){
-    //   for(var j=0; j<cols;j++){
-    //     grid[i][j].show(color(225, 225, 225));
-    //   }
-    // }
-    start.show(color(200,50,50));
-    end.show(color(200,50,50));
-
-    noFill();
-    stroke(255);
-    strokeWeight(cellDimentions/2);
-    beginShape();
-    for (var i = 0; i < path.length; i++) {
-      vertex(path[i].i *cellDimentions+  cellDimentions/ 2, path[i].j * cellDimentions +cellDimentions / 2);
-    }
-    endShape();
-
-
   }
+  for (var i = 0; i < closedSet.length; i++) {
+    closedSet[i].show(color(0, 205, 0));
+  }
+
+  for (var i = 0; i < openSet.length; i++) {
+    openSet[i].show(color(205, 205, 0));
+  }
+
+  // for (var i=3;i<rows;i++){
+  //   for(var j=0; j<cols;j++){
+  //     grid[i][j].show(color(225, 225, 225));
+  //   }
+  // }
+  start.show(color(200,50,50));
+  end.show(color(200,50,50));
+
+  noFill();
+  stroke(255);
+  strokeWeight(cellDimentions/2);
+  beginShape();
+  for (var i = 0; i < path.length; i++) {
+    vertex(path[i].j *cellDimentions+  cellDimentions/ 2, path[i].i * cellDimentions +cellDimentions / 2);
+  }
+  endShape();
+
+
+}
--- spot.js
+++ spot.js
@@ -7,78 +7,81 @@
 
 // An object to describe a spot in the grid
 function Spot(i, j) {
-    // Location
-    this.i = i;
-    this.j = j;
-  
-    // f, g, and h values for A*
-    this.f = 0;
-    this.g = 0;
-    this.h = 0;
-  
-    // Neighbors
-    this.neighbors = [];
-  
-    // Where did I come from?
-    this.previous = undefined;
-    this.visited=false;
-  
-    // Am I a wall?
-    this.wall = false;
-    if (random(1) < 0.2) {
-      this.wall = true;
-    }
-    // if(this.i==3 && this.j>3){
-    //   this.wall = true;
-    // }
-    // if(this.j==3 && this.i>=3){
-    //   this.wall = true;
-    // }
-  
-    // Display me
-    this.show = function(col) {
-      if (this.wall) {
-       noStroke();
-        fill('rgb(45, 45, 45)');
-      
-        rect(this.i * cellDimentions,this.j * cellDimentions, cellDimentions);
-        //ellipse(this.i * w + w / 2, this.j * h + h / 2, w / 2, h / 2);
-      } else if (col) {
-        strokeWeight(1);
-        stroke(50);
-        fill(col);
-        rect(this.j * cellDimentions,this.i * cellDimentions, cellDimentions);
-      //  console.log(this.i*cellDimentions,this.j*cellDimentions);
-      }
-    };
-  
-    // Figure out who my neighbors are
-    this.addNeighbors = function(grid) {
-      var i = this.i;
-      var j = this.j;
-      if (i < rows - 1) {
-        this.neighbors.push(grid[i + 1][j]);
-      }
-      if (i > 0) {
-        this.neighbors.push(grid[i - 1][j]);
-      }
-      if (j < cols - 1) {
-        this.neighbors.push(grid[i][j + 1]);
-      }
-      if (j > 0) {
-        this.neighbors.push(grid[i][j - 1]);
-      }
-      // if (i > 0 && j > 0) {
-      //   this.neighbors.push(grid[i - 1][j - 1]);
-      // }
-      // if (i < rows - 1 && j > 0) {
-      //   this.neighbors.push(grid[i + 1][j - 1]);
-      // }
-      // if (i > 0 && j < cols - 1) {
-      //   this.neighbors.push(grid[i - 1][j + 1]);
-      // }
-      // if (i < rows - 1 && j < cols - 1) {
-      //   this.neighbors.push(grid[i + 1][j + 1]);
-      // }
-    }
+  // Location
+  this.i = i;
+  this.j = j;
+
+  // f, g, and h values for A*
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+
+  // Neighbors
+  this.neighbors = [];
+
+  // Where did I come from?
+  this.previous = undefined;
+  this.visited=false;
+
+  // Am I a wall?
+  this.wall = false;
+  if (random(1) < 0.2) {
+    this.wall = true;
   }
+  // if(this.i==3 && this.j>3){
+  //   this.wall = true;
+  // }
+  // if(this.j==3 && this.i>=3){
+  //   this.wall = true;
+  // }
+
+  // Display me
+  this.show = function(col) {
+    if (this.wall) {
+     noStroke();
+      fill('rgb(45, 45, 45)');
+    
+      rect(this.j * cellDimentions,this.i * cellDimentions, cellDimentions);
+      
+      console.log(this.i * cellDimentions,this.j * cellDimentions);
+
+    } else if (col) {
+      strokeWeight(1);
+      stroke(50);
+      fill(col);
+      
+      rect(this.j * cellDimentions,this.i * cellDimentions, cellDimentions);
+    //  console.log(this.i*cellDimentions,this.j*cellDimentions);
+    }
+  };
+
+  // Figure out who my neighbors are
+  this.addNeighbors = function(grid) {
+    var i = this.i;
+    var j = this.j;
+    if (i < rows - 1) {
+      this.neighbors.push(grid[i + 1][j]);
+    }
+    if (i > 0) {
+      this.neighbors.push(grid[i - 1][j]);
+    }
+    if (j < cols - 1) {
+      this.neighbors.push(grid[i][j + 1]);
+    }
+    if (j > 0) {
+      this.neighbors.push(grid[i][j - 1]);
+    }
+    // if (i > 0 && j > 0) {
+    //   this.neighbors.push(grid[i - 1][j - 1]);
+    // }
+    // if (i < rows - 1 && j > 0) {
+    //   this.neighbors.push(grid[i + 1][j - 1]);
+    // }
+    // if (i > 0 && j < cols - 1) {
+    //   this.neighbors.push(grid[i - 1][j + 1]);
+    // }
+    // if (i < rows - 1 && j < cols - 1) {
+    //   this.neighbors.push(grid[i + 1][j + 1]);
+    // }
+  }
+}
